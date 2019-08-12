9a1b21e61a58fd8eab0dc16f03eb86aaf339c7ba
Philippe Beck <philippe@philippebeck.net>
2019-08-12 17:27:01 +0200

    Add all needed models

---
+++ src/Model/GlobalModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\Model;
+
+/**
+ * Class GlobalModel
+ * @package App\Model
+ */
+class GlobalModel extends Model {}
---
+++ src/Model/StatesModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\Model;
+
+/**
+ * Class StatesModel
+ * @package App\Model
+ */
+class StatesModel extends Model {}

64358abfec265790ce13899a761d0e142a65e864
Philippe Beck <philippe@philippebeck.net>
2019-08-17 02:30:29 +0200

    Update create tables file

--- config/create-table-animadio.sql
+++ config/create-table-animadio.sql
@@ -3,46 +3,48 @@ CREATE DATABASE animadio CHARACTER SET utf8;
 
 USE animadio;
 
-CREATE TABLE Media
+CREATE TABLE Part
 (
-    id              TINYINT         UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
-    name            CHAR(2)         NOT NULL    UNIQUE,
-    breakpoint      VARCHAR(10)     NOT NULL    UNIQUE
+    id          TINYINT         UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
+    part        VARCHAR(10)     NOT NULL    UNIQUE
 )
     ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-CREATE TABLE State
+CREATE TABLE Source
 (
     id          TINYINT         UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
-    launcher    VARCHAR(20)     NOT NULL    UNIQUE,
-    pivot       VARCHAR(10)     NOT NULL,
-    target      VARCHAR(20)     NOT NULL    UNIQUE
+    source      VARCHAR(10)     NOT NULL    UNIQUE
 )
     ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE Property
 (
-    id      TINYINT         UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
-    name    VARCHAR(30)     NOT NULL    UNIQUE
+    id          TINYINT         UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
+    property    VARCHAR(30)     NOT NULL    UNIQUE
 )
     ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE Variable
 (
-    id      SMALLINT        UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
-    name    VARCHAR(50)     NOT NULL    UNIQUE
+    id          SMALLINT        UNSIGNED    PRIMARY KEY     AUTO_INCREMENT,
+    variable    VARCHAR(50)     NOT NULL    UNIQUE
 )
     ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE Class
 (
-    id              SMALLINT                UNSIGNED        PRIMARY KEY     AUTO_INCREMENT,
-    name            VARCHAR(20)             NOT NULL        UNIQUE,
-    property_id     TINYINT                 UNSIGNED        NOT NULL,
-    variable_id     SMALLINT                UNSIGNED        NOT NULL,
-    state           TINYINT                 UNSIGNED        NOT NULL,
-    media           TINYINT                 UNSIGNED        NOT NULL,
-    CONSTRAINT      selector_property_id    FOREIGN KEY     (property_id)   REFERENCES  Property(id),
-    CONSTRAINT      selector_variable_id    FOREIGN KEY     (variable_id)   REFERENCES  Variable(id)
+    id              SMALLINT            UNSIGNED        PRIMARY KEY     AUTO_INCREMENT,
+    class           VARCHAR(20)         NOT NULL        UNIQUE,
+    part_id         TINYINT             UNSIGNED        NOT NULL,
+    source_id       TINYINT             UNSIGNED        NOT NULL,
+    property_id     TINYINT             UNSIGNED        NOT NULL,
+    variable_id     SMALLINT            UNSIGNED        NOT NULL,
+    media           BOOLEAN             NOT NULL,
+    concat          BOOLEAN             NOT NULL,
+    state           BOOLEAN             NOT NULL,
+    CONSTRAINT      fk_part_id          FOREIGN KEY     (part_id)       REFERENCES  Part(id),
+    CONSTRAINT      fk_source_id        FOREIGN KEY     (source_id)     REFERENCES  Source(id),
+    CONSTRAINT      fk_property_id      FOREIGN KEY     (property_id)   REFERENCES  Property(id),
+    CONSTRAINT      fk_variable_id      FOREIGN KEY     (variable_id)   REFERENCES  Variable(id)
 )
     ENGINE=INNODB DEFAULT CHARSET=UTF8;

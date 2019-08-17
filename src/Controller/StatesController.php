855531b24c038ae978bc5502cfbd6991df23eb7b
Philippe Beck <philippe@philippebeck.net>
2019-08-17 02:33:40 +0200

    Set states arrays

--- src/Controller/StatesController.php
+++ src/Controller/StatesController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Pam\Controller\Controller;
+use Pam\Model\ModelFactory;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
-use Pam\Controller\Controller;
 
 /**
  * Class StatesController
@@ -21,6 +22,67 @@ class StatesController extends Controller
      */
     public function IndexAction()
     {
-        return $this->render('states.twig');
+        $allStatesClasses = ModelFactory::getModel('Class')->listClasses(4);
+
+        $allAnimaClasses    = array();
+        $allBgClasses       = array();
+        $allBordClasses     = array();
+        $allColorClasses    = array();
+        $allDecoClasses     = array();
+        $allDisplayClasses  = array();
+        $allFlowClasses     = array();
+        $allPositionClasses = array();
+        $allShaboxClasses   = array();
+        $allShatexClasses   = array();
+
+        foreach ($allStatesClasses as $statesClass) {
+            $source = $statesClass['source'];
+
+            switch ($source) {
+                case 'anima':
+                    $allAnimaClasses[] = $statesClass;
+                    break;
+                case 'bg':
+                    $allBgClasses[] = $statesClass;
+                    break;
+                case 'bord':
+                    $allBordClasses[] = $statesClass;
+                    break;
+                case 'color':
+                    $allColorClasses[] = $statesClass;
+                    break;
+                case 'deco':
+                    $allDecoClasses[] = $statesClass;
+                    break;
+                case 'display':
+                    $allDisplayClasses[] = $statesClass;
+                    break;
+                case 'flow':
+                    $allFlowClasses[] = $statesClass;
+                    break;
+                case 'position':
+                    $allPositionClasses[] = $statesClass;
+                    break;
+                case 'shabox':
+                    $allShaboxClasses[] = $statesClass;
+                    break;
+                case 'shatex':
+                    $allShatexClasses[] = $statesClass;
+                    break;
+            }
+        }
+
+        return $this->render('states.twig', [
+            'allAnimaClasses'       => $allAnimaClasses,
+            'allBgClasses'          => $allBgClasses,
+            'allBordClasses'        => $allBordClasses,
+            'allColorClasses'       => $allColorClasses,
+            'allDecoClasses'        => $allDecoClasses,
+            'allDisplayClasses'     => $allDisplayClasses,
+            'allFlowClasses'        => $allFlowClasses,
+            'allPositionClasses'    => $allPositionClasses,
+            'allShaboxClasses'      => $allShaboxClasses,
+            'allShatexClasses'      => $allShatexClasses,
+        ]);
     }
 }

6565dfc3b7fe77b9b984d84914151e8f399e127a
Philippe Beck <philippe@philippebeck.net>
2019-08-18 16:59:12 +0200

    Create ElementsController

--- src/Controller/ElementsController.php
+++ src/Controller/ElementsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Pam\Controller\Controller;
+use Pam\Model\ModelFactory;
+use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
-use Pam\Controller\Controller;
 
 /**
  * Class ElementsController
@@ -14,6 +16,89 @@ use Pam\Controller\Controller;
 class ElementsController extends Controller
 {
     /**
+     * @var array
+     */
+    private $allElementsClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allBtnClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allCardClasses  = array();
+
+    /**
+     * @var array
+     */
+    private $allFootClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allFormClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allGalleryClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allMenuClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allNavbarClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allSliderClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allTableClasses = array();
+
+    /**
+     * StatesController constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        parent::__construct($twig);
+        $this->allElementsClasses = ModelFactory::getModel('Class')->listClasses(3);
+
+        foreach ($this->allElementsClasses as $elementsClass) {
+            switch ($elementsClass['source']) {
+                case 'btn': $this->allBtnClasses[] = $elementsClass;
+                    break;
+                case 'card': $this->allCardClasses[] = $elementsClass;
+                    break;
+                case 'foot': $this->allFootClasses[] = $elementsClass;
+                    break;
+                case 'form': $this->allFormClasses[] = $elementsClass;
+                    break;
+                case 'gallery': $this->allGalleryClasses[] = $elementsClass;
+                    break;
+                case 'menu': $this->allMenuClasses[] = $elementsClass;
+                    break;
+                case 'navbar': $this->allNavbarClasses[] = $elementsClass;
+                    break;
+                case 'slider': $this->allSliderClasses[] = $elementsClass;
+                    break;
+                case 'table': $this->allTableClasses[] = $elementsClass;
+                    break;
+            }
+        }
+    }
+
+    /**
      * @return string
      * @throws LoaderError
      * @throws RuntimeError
@@ -21,6 +106,16 @@ class ElementsController extends Controller
      */
     public function IndexAction()
     {
-        return $this->render('elements.twig');
+        return $this->render('elements.twig', [
+            'allBtnClasses'         => $this->allBtnClasses,
+            'allCardClasses'        => $this->allCardClasses,
+            'allFootClasses'        => $this->allFootClasses,
+            'allFormClasses'        => $this->allFormClasses,
+            'allGalleryClasses'     => $this->allGalleryClasses,
+            'allMenuClasses'        => $this->allMenuClasses,
+            'allNavbarClasses'      => $this->allNavbarClasses,
+            'allSliderClasses'      => $this->allSliderClasses,
+            'allTableClasses'       => $this->allTableClasses
+        ]);
     }
 }

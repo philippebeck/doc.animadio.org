369a71853940f0840e259c20ecb010e467718c3f
Philippe Beck <philippe@philippebeck.net>
2020-04-29 19:36:25 +0200

    Create box controller

---
+++ src/Controller/BoxController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Pam\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class BoxController
+ * @package App\Controller
+ */
+class BoxController extends MainController
+{
+    /**
+     * @var array
+     */
+    private $allContainerClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allMarginClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allBorderClasses  = array();
+
+    /**
+     * @var array
+     */
+    private $allPaddingClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allHeightClasses = array();
+
+    /**
+     * @var array
+     */
+    private $allWidthClasses  = array();
+
+    /**
+     * BoxController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $allBoxClasses = ModelFactory::getModel('Class')->listClasses(1);
+
+        foreach ($allBoxClasses as $boxClass) {
+            switch ($boxClass['source']) {
+                case 'container': $this->allContainerClasses[] = $boxClass;
+                    break;
+                case 'margin': $this->allMarginClasses[] = $boxClass;
+                    break;
+                case 'border': $this->allBorderClasses[] = $boxClass;
+                    break;
+                case 'padding': $this->allPaddingClasses[] = $boxClass;
+                    break;
+                case 'height': $this->allHeightClasses[] = $boxClass;
+                    break;
+                case 'width': $this->allWidthClasses[] = $boxClass;
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render('box.twig', [
+            'allContainerClasses'   => $this->allContainerClasses,
+            'allMarginClasses'      => $this->allMarginClasses,
+            'allBorderClasses'      => $this->allBorderClasses,
+            'allPaddingClasses'     => $this->allPaddingClasses,
+            'allHeightClasses'      => $this->allHeightClasses,
+            'allWidthClasses'       => $this->allWidthClasses
+        ]);
+    }
+}

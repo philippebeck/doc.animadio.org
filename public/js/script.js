011523fd33cc465a8256012787877eadecdad27d
Philippe Beck <philippe@philippebeck.net>
2020-05-06 16:42:35 +0200

    Update arguments to animadio.js calls

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,29 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  var animadio = new Animadio(
+  var anima = new Animadio(
       ["name", "function", "count", "direction"],
-      "anima",
+      ["anima", ["check", "hub", "goal"]],
       [2000, {few: 2, many: 5, loop: 30}]
   );
 
-  var display   = new Animadio(["display"], "display");
-  var position  = new Animadio(["position"], "position");
+  var display = new Animadio(
+      ["display"],
+      ["display", ["check", "hub", "goal"]]
+  );
 
-  var bg    = new Animadio(["bg"], "bg");
-  var color = new Animadio(["color"], "color");
+  var position = new Animadio(
+      ["position"],
+      ["position", ["check", "hub", "goal"]]
+  );
+
+  var bg = new Animadio(
+      ["bg"],
+      ["bg", ["check", "hub", "goal"]]
+  );
+
+  var color = new Animadio(
+      ["color"],
+      ["color", ["check", "hub", "goal"]]
+  );
 });

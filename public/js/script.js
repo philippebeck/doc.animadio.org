8ed91cf2973b09ba697af0015764429584d42845
Philippe Beck <philippe@philippebeck.net>
2020-05-07 18:10:55 +0200

    Update script for Input classes

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,28 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  var anima = new Animadio(
+  var anima = new Input(
       ["name", "function", "count", "direction"],
       ["anima", ["check", "hub", "goal"]],
       [2000, {few: 2, many: 5, loop: 30}]
   );
 
-  var display = new Animadio(
+  var display = new Input(
       ["display"],
       ["display", ["check", "hub", "goal"]]
   );
 
-  var position = new Animadio(
+  var position = new Input(
       ["position"],
       ["position", ["check", "hub", "goal"]]
   );
 
-  var bg = new Animadio(
+  var bg = new Input(
       ["bg"],
       ["bg", ["check", "hub", "goal"]]
   );
 
-  var color = new Animadio(
+  var color = new Input(
       ["color"],
       ["color", ["check", "hub", "goal"]]
   );

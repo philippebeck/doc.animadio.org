592169ce0c2a7d817455fc68757abd17e1688b27
Philippe Beck <philippe@philippebeck.net>
2020-05-15 21:44:19 +0200

    Create events controller

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class EventsController
+ * @package App\Controller
+ */
+class EventsController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render('main/events.twig');
+    }
+}

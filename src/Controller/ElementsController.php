09ad98ab8e3410794a826bd3f1ed845fc1f66abb
Philippe Beck <philippe@philippebeck.net>
2020-05-20 09:04:07 +0200

    Remove useless php code to elements controller

--- src/Controller/ElementsController.php
+++ src/Controller/ElementsController.php
@@ -15,84 +15,6 @@ use Twig\Error\SyntaxError;
 class ElementsController extends MainController
 {
     /**
-     * @var array
-     */
-    private $allBtnClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allCardClasses  = array();
-
-    /**
-     * @var array
-     */
-    private $allFootClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allFormClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allGalleryClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allMenuClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allNavbarClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allTableClasses = array();
-
-    /**
-     * StatesController constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $allElementsClasses = ModelFactory::getModel('Class')->listClasses(3);
-
-        foreach ($allElementsClasses as $elementsClass) {
-            $this->getClass($elementsClass);
-        }
-    }
-
-    /**
-     * @param array $class
-     */
-    private function getClass(array $class)
-    {
-        switch ($class['source']) {
-            case 'btn': $this->allBtnClasses[] = $class;
-                break;
-            case 'card': $this->allCardClasses[] = $class;
-                break;
-            case 'foot': $this->allFootClasses[] = $class;
-                break;
-            case 'form': $this->allFormClasses[] = $class;
-                break;
-            case 'gallery': $this->allGalleryClasses[] = $class;
-                break;
-            case 'menu': $this->allMenuClasses[] = $class;
-                break;
-            case 'navbar': $this->allNavbarClasses[] = $class;
-                break;
-            case 'table': $this->allTableClasses[] = $class;
-                break;
-        }
-    }
-
-    /**
      * @return string
      * @throws LoaderError
      * @throws RuntimeError
@@ -100,15 +22,6 @@ class ElementsController extends MainController
      */
     public function defaultMethod()
     {
-        return $this->render('main/elements.twig', [
-            'allBtnClasses'         => $this->allBtnClasses,
-            'allCardClasses'        => $this->allCardClasses,
-            'allFootClasses'        => $this->allFootClasses,
-            'allFormClasses'        => $this->allFormClasses,
-            'allGalleryClasses'     => $this->allGalleryClasses,
-            'allMenuClasses'        => $this->allMenuClasses,
-            'allNavbarClasses'      => $this->allNavbarClasses,
-            'allTableClasses'       => $this->allTableClasses
-        ]);
+        return $this->render('main/elements.twig');
     }
 }

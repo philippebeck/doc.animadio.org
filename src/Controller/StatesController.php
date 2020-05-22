0be64f30a299de80e1203c41459511ee689071df
Philippe Beck <philippe@philippebeck.net>
2020-05-22 19:40:17 +0200

    Update StatesController for new states DB

--- src/Controller/StatesController.php
+++ src/Controller/StatesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Pam\Controller\MainController;
 use Pam\Model\Factory\ModelFactory;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -12,65 +11,8 @@ use Twig\Error\SyntaxError;
  * Class StatesController
  * @package App\Controller
  */
-class StatesController extends MainController
+class StatesController extends BaseController
 {
-    /**
-     * @var array
-     */
-    private $allAnimaClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allDisplayClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allPositionClasses = array();
-
-    /**
-     * @var array
-     */
-    private $allBgClasses  = array();
-
-    /**
-     * @var array
-     */
-    private $allColorClasses = array();
-
-    /**
-     * StatesController constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $allStatesClasses = ModelFactory::getModel('Class')->listClasses(3);
-
-        foreach ($allStatesClasses as $statesClass) {
-            $this->getClass($statesClass);
-        }
-    }
-
-    /**
-     * @param array $class
-     */
-    private function getClass(array $class)
-    {
-        switch ($class['source']) {
-            case 'anima': $this->allAnimaClasses[] = $class;
-                break;
-            case 'display': $this->allDisplayClasses[] = $class;
-                break;
-            case 'position': $this->allPositionClasses[] = $class;
-                break;
-            case 'bg': $this->allBgClasses[] = $class;
-                break;
-            case 'color': $this->allColorClasses[] = $class;
-                break;
-        }
-    }
-
     /**
      * @return string
      * @throws LoaderError
@@ -79,12 +21,15 @@ class StatesController extends MainController
      */
     public function defaultMethod()
     {
+        $statesClassesList  = ModelFactory::getModel('States')->listStatesClasses();
+        $statesClasses      = $this->getClasses($statesClassesList);
+
         return $this->render('main/states.twig', [
-            'allAnimaClasses'       => $this->allAnimaClasses,
-            'allDisplayClasses'     => $this->allDisplayClasses,
-            'allPositionClasses'    => $this->allPositionClasses,
-            'allBgClasses'          => $this->allBgClasses,
-            'allColorClasses'       => $this->allColorClasses
+            'animaClasses'      => $statesClasses[1],
+            'displayClasses'    => $statesClasses[2],
+            'positionClasses'   => $statesClasses[3],
+            'bgClasses'         => $statesClasses[4],
+            'colorClasses'      => $statesClasses[5]
         ]);
     }
 }
